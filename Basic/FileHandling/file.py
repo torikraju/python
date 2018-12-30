0ba9bf6be21c126b472b16df173a2cdc8202d6b5
torikraju <torikraju@hotmail.com>
2018-12-30 13:32:54 +0600

    Lesson 104. Using the with Block Statement

--- Basic/FileHandling/file.py
+++ Basic/FileHandling/file.py
@@ -10,8 +10,6 @@
 file_name = 'demo.txt'
 
 # writing to file
-
-
 def write_to_file(content, file_name):
     f = open(file_name, mode='w')
     f.write(content+'\n')
@@ -39,10 +37,16 @@ def read_multiple_line_from_file(file_name):
     f.close()
     return file_content
 
+# read from file with keyword
+def read_file_with(file_name):
+    with open(file_name, mode='r') as f:
+        file_content = f.readlines()
+        return file_content
+
 
 write_to_file('Hello from python', file_name)
 append_to_file('append this line', file_name)
 print(read_single_from_file(file_name))
 print(read_multiple_line_from_file(file_name))
 print([el[:-1] for el in read_multiple_line_from_file(file_name)])
-
+print(read_file_with(file_name))
