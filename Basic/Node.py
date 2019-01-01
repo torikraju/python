51f85cbf192f9a6a7fab9041633bb7be2bfca0f8
torikraju <torikraju@hotmail.com>
2019-01-01 15:15:57 +0600

    Lesson 171. Adding Transactions

--- Basic/Node.py
+++ Basic/Node.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 from Wallet import Wallet
@@ -76,6 +76,51 @@ def get_chain():
     for dict_block in dict_chain:
         dict_block['transactions'] = [tx.__dict__ for tx in dict_block['transactions']]
     return jsonify(dict_chain), 200
+
+
+@app.route('/transaction', methods=['POST'])
+def add_transaction():
+    if wallet.public_key is None:
+        response = {
+            'message': 'No wallet setup.'
+        }
+        return jsonify(response), 400
+
+    values = request.get_json()
+    if not values:
+        response = {
+            'message': 'No data found.'
+        }
+        return jsonify(response), 400
+
+    required_filed = ['recipient', 'amount']
+    if not all(field in values for field in required_filed):
+        response = {
+            'message': 'Required data is missing.'
+        }
+        return jsonify(response), 400
+
+    recipient = values['recipient']
+    amount = values['amount']
+    signature = wallet.sign_transaction(wallet.public_key, recipient, amount)
+    success = blockchain.add_transaction(recipient, wallet.public_key, signature, amount)
+    if success:
+        response = {
+            'message': 'Successfully added transaction.',
+            'transaction': {
+                'sender': wallet.public_key,
+                'recipient': recipient,
+                'amount': amount,
+                'signature': signature
+            },
+            'funds': blockchain.get_balance()
+        }
+        return jsonify(response), 201
+    else:
+        response = {
+            'message': 'Creating a transaction failed.'
+        }
+        return jsonify(response), 500
 
 
 @app.route('/mine', methods=['POST'])
