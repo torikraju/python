ba77404fa6daec68a1bb59748c7a4ec2795c2eed
torikraju <torikraju@hotmail.com>
2019-01-01 14:22:19 +0600

    Lesson 169. Setting Up Routes to Manage our Wallet

--- Basic/Wallet.py
+++ Basic/Wallet.py
@@ -22,8 +22,10 @@ class Wallet:
                     f.write(self.public_key)
                     f.write('\n')
                     f.write(self.private_key)
+                    return True
             except (IOError, IndexError):
                 print('Saving wallet failed...')
+                return False
 
     def load_keys(self):
         try:
@@ -31,10 +33,13 @@ class Wallet:
                 keys = f.readlines()
                 self.public_key = keys[0][:-1]
                 self.private_key = keys[1]
+                return True
         except (IOError, IndexError):
             print('Loading wallet failed...')
+            return False
 
-    def generate_keys(self):
+    @staticmethod
+    def generate_keys():
         private_key = RSA.generate(1024, Crypto.Random.new().read)
         public_key = private_key.publickey()
         return binascii.hexlify(private_key.exportKey(format="DER")).decode(), binascii.hexlify(
